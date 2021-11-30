00b61899db8da891d64339889555d2b8ad463dd4
rao-bushra21 <95148935+rao-bushra21@users.noreply.github.com>
2021-11-30 12:26:28 +0500

    Lab

---
+++ Bushra/ConsoleApplication1/Interface1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public interface Interface1
+    {
+        int goodone { get; set; }
+
+        void newone();
+    }
+}
